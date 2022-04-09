9139c116f575892c5e5338cd65c5270070c4d955
JinleiGu <s1315289@monmouth.edu>
2022-04-10 06:31:28 +0800

    add storge

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "react": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "imagestorge": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
