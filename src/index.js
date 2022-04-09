3746710727fb2225f59da599582c9a2f9dcfb554
JinleiGu <s1315289@monmouth.edu>
2022-04-10 06:51:01 +0800

    fix auth

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig  from './aws-exports';
 
-Amplify.configure(config);
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
